e106f37d0ba63648e44a96dbbf78cd3887fce8b4
zachm17 <zachmoy17@gmail.com>
2015-10-21 08:29:30 -0600

    Created runner, controller, and copied Chatbot.java from canvas.

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,18 @@
+package chat.controller;
+
+/**
+ * Starts the Chatbot Programs.
+ * @author Zach Moyes
+ * 1.0 10/21/15
+ */
+
+public class ChatbotRunner
+{
+	public static void main(String [] args)
+	{
+		
+	ChatbotController botController = new ChatbotController();
+	botController.start();
+	
+	}
+}
