a9adf68c8bac7e6a96cb0bd7961afe27748eb439
zachm17 <zachmoy17@gmail.com>
2015-10-23 08:20:02 -0600

    addd lengthchecker

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -42,7 +42,22 @@ public class Chatbot
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
-		return false;
+		boolean hasLength = false;
+		
+		if(currentInput != null)
+		{
+			if(currentInput.length() >= 1)
+			{
+				hasLength = true;
+			}
+		}
+		
+		if(currentInput !=null  && currentInput.length() > 0)
+		{
+			hasLength = true;
+		}
+		
+		return hasLength;
 	}
 	
 	/**
