6c5c204cec07dcd0d477ef03fe4fc4901f9daa14
Zach <zachmoy17@gmail.com>
2015-11-19 15:19:06 -0700

    Added all the chatbot tests

---
+++ ChatTests.java
@@ -0,0 +1,16 @@
+package chat.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ChatbotTest.class, 
+	ChatControllerTest.class, 
+	ChatFrameTest.class, 
+	ChatPanelTest.class })
+public class ChatTests
+{
+
+}
