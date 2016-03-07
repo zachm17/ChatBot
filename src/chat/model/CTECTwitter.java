c49f99aa62dcc5e2f11224376d9dbc73a2df775a
zachm17 <zachmoy17@gmail.com>
2016-03-07 09:15:13 -0700

    Added method for sendTweet

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -3,10 +3,11 @@ package chat.model;
 import twitter4j.*;
 import java.util.ArrayList;
 
+
 /**
  * 
  * @author Zach Moyes
- * @version 0.3 added statusList to constructor
+ * @version 0.4 copy pastad sendtweet method
  * @description CTEC TWITTER BABAY
  */
 public class CTECTwitter
@@ -14,10 +15,18 @@ public class CTECTwitter
 	//Declaration
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
 	
 	public CTECTwitter()
 	{
 		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+		
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
 	}
 	
 	
