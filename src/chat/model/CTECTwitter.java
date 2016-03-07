6dd7f42981e5d6fda5144dc342d0ebe7b86b6790
zachm17 <zachmoy17@gmail.com>
2016-03-07 08:59:41 -0700

    Added declaration section T4J

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,17 @@
 package chat.model;
 
+import twitter4j.*;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Zach Moyes
+ * @version 0.2 Ref  T4J
+ * @description CTEC TWITTER BABAY
+ */
 public class CTECTwitter
 {
-
+	//Declaration
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
 }
