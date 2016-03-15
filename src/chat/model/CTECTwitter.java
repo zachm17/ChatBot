dc1af5d614e23a9c59d678fecccb45de32ec05d5
zachm17 <zachmoy17@gmail.com>
2016-03-15 09:36:05 -0600

    added topResults method to CTECTwitter

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -156,6 +156,37 @@ public class CTECTwitter
 		return boringWords;
 	}
 	
+	public String topResults(List<String> wordList)
+	{
+		String tweetResults = "";
+		
+		int topWordLocation = 0;
+		
+		int topCount = 0;
+		
+		for(int index = 0; index < wordList.size(); index++)
+		{
+			int wordUseCount = 1; //Inside the loop so that we dont have to reset it everytime
+			
+			for(int spot = index + 1; spot < wordList.size(); spot++)
+			{
+				if(wordList.get(index).equals(wordList.get(spot)))
+				{
+					wordUseCount++;
+				}
+				
+				if(wordUseCount > topCount)
+				{
+					topCount = wordUseCount;
+					topWordLocation = index;
+				}
+			}   
+		}
+		
+		tweetResults = "The top word in teh tweets was " + wordList.get(topWordLocation) + " and it was used " + topCount + " times!";
+		return tweetResults;
+	}
+	
 }
 
 
