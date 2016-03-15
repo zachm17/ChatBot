e5b4e7b56ceecd83b804b269b2e76286f9d734fc
zachm17 <zachmoy17@gmail.com>
2016-03-15 09:45:18 -0600

    removed List<String> wordList from topResults

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -156,7 +156,7 @@ public class CTECTwitter
 		return boringWords;
 	}
 	
-	public String topResults(List<String> wordList)
+	public String topResults()
 	{
 		String tweetResults = "";
 		
@@ -186,6 +186,7 @@ public class CTECTwitter
 		tweetResults = "The top word in teh tweets was " + wordList.get(topWordLocation) + " and it was used " + topCount + " times!";
 		return tweetResults;
 	}
+	
 	
 }
 
